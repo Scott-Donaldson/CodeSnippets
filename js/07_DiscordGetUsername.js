451846e4b6f48a35ddc7b3ff5a58ffb5621bc338
Scrub <43659841+Scott-Donaldson@users.noreply.github.com>
2020-12-26 14:54:01 +0000

    Create 07_DiscordGetUsername.js

---
+++ js/07_DiscordGetUsername.js
@@ -0,0 +1,5 @@
+getUser = function(message, position) {
+    var userArr = message.match(/([@-])[\w-]+/g);
+    if(position > userArr.length) return "";
+    return userArr[position - 1].substring(1);
+};
