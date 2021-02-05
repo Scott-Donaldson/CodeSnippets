23b7070b4cbab9b0ff6e2717f9be2afc300232ef
Scrub <scott-donaldson@outlook.com>
2021-02-04 20:41:19 -0800

    Updated Dice C program

--- c/dice.c
+++ c/dice.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
+int main(int argc, char** argv){
     int diceRoll(void);
-    int returnDiceRolls(int *array, int *x);
+    void returnDiceRolls(int *array, int *x);
 
     int *rolls;
-    printf("How many rolls of a dice?: ");
-    scanf("%d", rolls);
+
+    if(argc > 1){
+        *rolls = (int) strtol(argv[1],(char **) NULL,10);
+    }else{
+        printf("How many rolls of a dice?: ");
+        scanf("%d", rolls);
+    }
+
     int diceRolls[*rolls];
     for(size_t i = 0; i < *rolls; i++){
         int roll = diceRoll();
@@ -22,10 +28,12 @@ int diceRoll (){
     return (random() % 6) + 1;
 }
 
-int returnDiceRolls(int *array, int *arrlen){
+void returnDiceRolls(int *array, int *arrlen){
     char output[(*arrlen * 4) + 100];
     strcat(output, "Results: ");
+
     int sum = 0;
+
     for(size_t i = 0; i < *arrlen; i++){
         int arrval = array[i];
         sum += arrval;
@@ -35,9 +43,11 @@ int returnDiceRolls(int *array, int *arrlen){
     }
     char* sumstr = "\nSum: ";
     char intchar[sizeof(sum)];
+
     strcat(output, sumstr);
     sprintf(intchar,"%d",sum);
     strcat(output, intchar);
     printf("%s\n",output);
-    return 0;
+
+    return;
 }
